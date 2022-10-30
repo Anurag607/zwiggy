12ebb5d6d098d9ec40bfc6e589e14c8b88cdb79f
Anurag <anurag79002@gmail.com>
2022-10-30 20:07:34 +0530

    feat(ui) : Initial

---
+++ ui/src/components/cards/cards.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './cards.module.css'
+
+const Cards = () => {
+    return (
+        <div className={styles.wrapper}>
+            <section></section>
+        </div>
+    )
+}
+
+export default Cards
