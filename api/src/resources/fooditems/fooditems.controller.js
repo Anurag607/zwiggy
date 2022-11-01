34cc0a8e441246e50ffe281a14baa7eef5857bfd
Aryan Shrivastava <aryanshrivastava237@gmail.com>
2022-11-02 00:19:31 +0530

    add fooditems endpoint

---
+++ api/src/resources/fooditems/fooditems.controller.js
@@ -0,0 +1,7 @@
+const fooditemsService = require('./fooditems.service');
+
+const getAllFoodItems = async (req, res) => {
+  const data = await fooditemsService.getAllFoodItems(req.params['name']);
+  res.status(200).json(JSON.parse(JSON.stringify(data)));
+}
+module.exports={getAllFoodItems}
---
+++ api/src/utils/fooditemsData.sql
@@ -0,0 +1,13 @@
+INSERT INTO fooditem ( item_name,price)
+VALUES ('Chicken Kebab Platter',400),
+('Margherita Pizza',150),
+('Chicken Wings',500),
+('Soup',80),
+('Pasta',170),
+('Fries',80),
+('Dosa',50),
+('Maggie',30),
+('Onion Pizza',95),
+('Cheese Pizza',100);
+
+    
