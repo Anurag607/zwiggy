b432f13ca1c273e5453ddaeeaa0506c32616ef91
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2022-11-08 01:49:24 +0530

    [api]: setup authentication (#26)
    * [api]: setup authentication
    
    * various changes

--- api/src/index.js
+++ api/src/index.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const restaurantRouter = require('./resources/restaurant/restaurant.router');
-const fooditemsRouter=require('./resources/fooditems/fooditems.router');
+const fooditemsRouter = require('./resources/fooditems/fooditems.router');
+const userRouter = require('./resources/user/user.router');
 
 const app = express();
 
 app.disable('x-powered-by');
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/restaurants', restaurantRouter);
-
 app.use('/api/fooditems',fooditemsRouter);
-
-// app.get("/", (req, res) => {
-//   res.send({ message: "hi there" });
-// });
+app.use('/api/user', userRouter);
 
 app.listen("3000");
