ee977f4fda0a3bedbda573aebba2b07c54d01953
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2022-11-08 02:17:20 +0530

    feat(api): setup authorization

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const auth = require('./utils/auth');
 const restaurantRouter = require('./resources/restaurant/restaurant.router');
 const fooditemsRouter = require('./resources/fooditems/fooditems.router');
 const userRouter = require('./resources/user/user.router');
@@ -12,8 +13,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/user', userRouter);
+app.use(auth.verifyToken);
 app.use('/api/restaurants', restaurantRouter);
 app.use('/api/fooditems',fooditemsRouter);
-app.use('/api/user', userRouter);
 
 app.listen("3000");
