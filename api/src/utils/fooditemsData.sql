b28fb2b05e7c0af212383baef5772d0aa90c8604
Aryan Shrivastava <107790721+aryan0723@users.noreply.github.com>
2022-11-08 19:34:26 +0530

    (api) add menu endpoint (#31)

--- api/src/utils/fooditemsData.sql
+++ api/src/utils/fooditemsData.sql
@@ -1,13 +1,11 @@
-INSERT INTO fooditem ( item_name,price)
-VALUES ('Chicken Kebab Platter',400),
-('Margherita Pizza',150),
-('Chicken Wings',500),
-('Soup',80),
-('Pasta',170),
-('Fries',80),
-('Dosa',50),
-('Maggie',30),
-('Onion Pizza',95),
-('Cheese Pizza',100);
-
-    
+INSERT INTO fooditem ( item_name,price,cat_id)
+VALUES ('Chicken Kebab Platter',400,34),
+('Margherita Pizza',150,34),
+('Chicken Wings',500,34),
+('Soup',80,35),
+('Pasta',170,35),
+('Fries',80,36),
+('Dosa',50,36),
+('Maggie',30,40),
+('Onion Pizza',95,40),
+('Cheese Pizza',100,40);
